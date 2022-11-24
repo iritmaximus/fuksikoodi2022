33976d73db461aaed48f575bcb17f65d34e3773a
kaariroo <kaariainenroope@gmail.com>
2022-11-24 18:32:27 +0200

    kauppa

---
+++ kauppa.py
@@ -0,0 +1,5 @@
+
+kauppa = ["deli", "kahvit", "kuivat", "juomat", "leivät", "leipomo", "mausteet", "kananmunat", "juustot", "rasvat", "maitotuotteet", "proteiinit", "hevi", "pakasteet", "makeiset"]
+
+for tuote in kauppalista:
+    if 
