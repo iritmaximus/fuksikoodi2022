ea78b5b00ed9bfb9ca82d957388df9dde443336c
Simo Sahlman <ssahlman1@gmail.com>
2022-11-24 19:50:23 +0200

    uusi muutos

--- reseptit.py
+++ reseptit.py
@@ -3,38 +3,23 @@ reseptit = []
 nakkimunakas = {
     "nimi": "nakkimunakas",
     "ainekset": [
-        {"nimi": "nakki", "määrä": 6, "yksikkö": "kpl", "osasto": "kylmätuotteet"},
-        {"nimi": "muna", "määrä": 2, "yksikkö": "kpl", "osasto": "kylmätuotteet"},
-    ],
+        {"nimi": "nakki", "määrä": 1, "yksikkö": "500 g paketti", "osasto": "kylmätuotteet"},
+        {"nimi": "muna", "määrä": 1, "yksikkö": "6 kpl paketti", "osasto": "kylmätuotteet"},
+    ]
 }
 reseptit.append(nakkimunakas)
 
 smoothie = {
     "nimi": "smoothie",
-    "ainekset": [{"omena": 2, "banaani": 2, "appelsiini": 1}],
-}
+    "ainekset": [{"nimi": "omena", "määrä": 2, "yksikkö": "kpl", "osasto": "heviosasto"}, 
+    {"nimi": "banaani", "määrä": 2, "yksikkö": "kpl", "osasto": "heviosasto"},
+    {"nimi": "appelsiini", "määrä": 1, "yksikkö": "kpl", "osasto": "heviosasto"}]}
+
 reseptit.append(smoothie)
 
-riisipuuro = {"nimi": "riisipuuro", "ainekset": [{"maito": 1, "riisi": 1}]}
+riisipuuro = {"nimi": "riisipuuro", "ainekset": [{"nimi": "riisi", "määrä": 1, "yksikkö": "500 g paketti", "osasto": "kuivaosasto"}, 
+    {"nimi": "maito", "määrä": 1, "yksikkö": "litra", "osasto": "kylmäosasto"}]}
+
 reseptit.append(riisipuuro)
 
-looppi = True
-
-kauppalista = []
-
-
-while looppi is True:
-    resepti = input("Anna resepti: ")
-    for x in reseptit:
-        if x["nimi"] == resepti:
-            print("Hienoa, resepti löytyi!")
-            for y in x["ainekset"]:
-                kauppalista.append(y)
-            looppi = False
-            break
-    else:
-        print("Valitettavasti reseptiä ei löytynyt")
-        print("Maistuisiko smoothie?")
-
-
-print(kauppalista)
+print(reseptit)
